9c8e67db374f2c16ac90954efafa71f4035ff2dc
Stuart Wilson <tuzgai@gmail.com>
2019-01-25 22:15:18 -0600

    Fix loading malformed lines

--- DvdCollection/src/main/java/com/sg/dvdcollection/dao/DvdCollectionDaoFileImpl.java
+++ DvdCollection/src/main/java/com/sg/dvdcollection/dao/DvdCollectionDaoFileImpl.java
@@ -17,11 +17,12 @@ import java.util.Scanner;
  *
  * @author Stuart
  */
-public class DvdCollectionDaoFileImpl implements DvdCollectionDao{
+public class DvdCollectionDaoFileImpl implements DvdCollectionDao {
+
     private Map<String, DVD> dvdCollection = new HashMap<>();
     public static final String COLLECTION_FILE = "dvd.txt";
     public static final String DELIMITER = "::";
-    
+
     @Override
     public DVD addDvd(String title, DVD dvd) throws DvdCollectionDaoException {
         DVD newDvd = dvdCollection.put(title, dvd);
@@ -47,51 +48,54 @@ public class DvdCollectionDaoFileImpl implements DvdCollectionDao{
         writeDvdCollection();
         return dvd;
     }
-    
+
     private void loadDvdCollection() throws DvdCollectionDaoException {
         Scanner scn;
-        
+
         try {
             scn = new Scanner(new File(COLLECTION_FILE));
         } catch (FileNotFoundException e) {
             throw new DvdCollectionDaoException("-_- Could not load collection file into memory.", e);
         }
-        
+
         String currentLine;
-        
+
         String[] currentTokens;
-        
+
         while (scn.hasNextLine()) {
             currentLine = scn.nextLine();
             currentTokens = currentLine.split(DELIMITER);
-            
-            DVD newDVD = new DVD(currentTokens[0]);
-            newDVD.setReleaseDate(LocalDate.ofEpochDay(Long.parseLong(currentTokens[1])));
-            newDVD.setRating(currentTokens[2]);
-            newDVD.setStudio(currentTokens[3]);
-            newDVD.setNote(currentTokens[4]);
-            
-            dvdCollection.put(newDVD.getTitle(), newDVD);
+
+            // Skip malformed lines
+            if (currentTokens.length == 5) {
+                DVD newDVD = new DVD(currentTokens[0]);
+                newDVD.setReleaseDate(LocalDate.ofEpochDay(Long.parseLong(currentTokens[1])));
+                newDVD.setRating(currentTokens[2]);
+                newDVD.setStudio(currentTokens[3]);
+                newDVD.setNote(currentTokens[4]);
+
+                dvdCollection.put(newDVD.getTitle(), newDVD);
+            }
         }
         scn.close();
     }
-    
+
     private void writeDvdCollection() throws DvdCollectionDaoException {
         PrintWriter out;
-        
+
         try {
             out = new PrintWriter(new FileWriter(COLLECTION_FILE));
         } catch (IOException e) {
             throw new DvdCollectionDaoException("Could not save collection data.", e);
         }
-        
+
         List<DVD> dvdList = this.getAllDvds();
-        for(DVD dvd : dvdList) {
-            out.println(dvd.getTitle() + DELIMITER +
-                    dvd.getReleaseDate().toEpochDay() + DELIMITER +
-                    dvd.getRating() + DELIMITER +
-                    dvd.getStudio() + DELIMITER +
-                    dvd.getNote());
+        for (DVD dvd : dvdList) {
+            out.println(dvd.getTitle() + DELIMITER
+                    + dvd.getReleaseDate().toEpochDay() + DELIMITER
+                    + dvd.getRating() + DELIMITER
+                    + dvd.getStudio() + DELIMITER
+                    + dvd.getNote());
             out.flush();
         }
         out.close();
