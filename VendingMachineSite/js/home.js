01cd8cf1cd8ab4ac25e602bdc42f4a8114712d59
Stuart Wilson <tuzgai@gmail.com>
2019-03-08 13:23:32 -0600

    Vending Machine: implement item selection

--- VendingMachineSite/js/home.js
+++ VendingMachineSite/js/home.js
@@ -14,13 +14,21 @@ $(document).ready(function() {
 function displayItems(itemArray) {
     $.each(itemArray, function(index, item) {
         var preparedHtml = 
-            '<button class="col-4 col card" onclick = selectItem(' + item.id + ')>' +
+            '<button class="col-4 col card" id="itemButton' + item.id + '" ' +  
+            'onclick = selectItem(' + item.id + ')>' +
             '<p class="text-left">' + item.id + '</p>' +
-            '<p class="text-center">' + item.name + '</p>' +
+            '<p class="text-center name">' + item.name + '</p>' +
             '<p class="text-center"> $' + item.price.toFixed(2) + '</p>' +
             '<p class="text-center"> Quantity left: ' + item.quantity + '</p>' +
             '</button>';
 
         $('.inventory').append(preparedHtml);
-    })
+    });
+}
+
+function selectItem(id) {
+    var button = $('#itemButton' + id);
+    $('.selected').removeClass('selected');
+    button.addClass('selected');
+    $('.itemDisplay').text(button.find('.name').text());
 }
