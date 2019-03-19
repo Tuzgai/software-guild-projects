1aac0c9e7b58deb769867960e208e4415ff4a2a3
Stuart Wilson <tuzgai@gmail.com>
2019-03-19 11:12:30 -0500

    HeroDB: first form template complete

--- herodb/src/main/resources/templates/scripts/scripts.js
+++ herodb/src/main/resources/templates/scripts/scripts.js
@@ -2,3 +2,17 @@ $(".expandable").click(function() {
     $(".shortText", this).toggle();
     $(".longText", this).toggleClass("visible");
 });
+
+/* Hero roster module */
+function removeHero() {
+    var item = $("#currentHeroes").children("option:selected");
+    $("#heroRoster").append(item);
+    $("#currentHeroes").remove(item);
+}
+
+function addHero() {
+    var item = $("#heroRoster").children("option:selected");
+    $("#currentHeroes").append(item);
+    $("#heroRoster").remove(item);
+}
+/* End Hero roster module */
